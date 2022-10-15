a5718a00b8915df04657f6346ad166e7877c350e
Sahil739 <sahilparsana98247@gmail.com>
2022-10-15 12:26:20 +0530

    First time all files will be added

---
+++ Module 1/Module1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    // My First Assignment
+
+    printf("My name is Sahil Parsana \n");
+    printf("I was born on 28th December 2003 \n");
+    printf("I am 18 years old \n");
+    printf("My Address is:- Plot No.- 1, Sargan Bunglow, Nr. Saurastra Township, Mota Varachha, Surat:- 394101, Gujarat \n");
+
+    return 0;
+}
